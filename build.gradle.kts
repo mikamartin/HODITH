d1c3b92e5ab57b40519ccd64f6e3c793d29d1833
SecondMonday Studios <secondmondaystudios@gmail.com>
2026-07-04 15:54:32 -0700

    chore: configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,4 +15,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
     id("com.google.dagger.hilt.android") version "2.60" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
 }
